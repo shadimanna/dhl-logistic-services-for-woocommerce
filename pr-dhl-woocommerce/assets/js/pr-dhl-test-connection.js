fa6a3a69f7d3ecc41652489b0a97a872c38b8663
antonk52 <antonk52@icloud.com>
2017-10-25 21:32:11 +0300

    clean test connection js

--- pr-dhl-woocommerce/assets/js/pr-dhl-test-connection.js
+++ pr-dhl-woocommerce/assets/js/pr-dhl-test-connection.js
@@ -1,51 +1,43 @@
 /**
  * Ajax call to sync orders!
  */
-function dhlTestConnection( btn_id ) {
-	var btn = jQuery( btn_id );
+function dhlTestConnection(btn_id) {
+  var $ = jQuery;
+  var btn = $(btn_id);
 
-	// Remove elements after button
-	btn.nextAll().remove();
+  // Remove elements after button
+  btn.nextAll().remove();
 
-	btn.attr("disabled", true);
-	btn.text('Testing Connection...');
+  btn.attr('disabled', true);
+  btn.text('Testing Connection...');
 
-	var loaderContainer = jQuery( '<span/>', {
-        'class': 'loader-image-container'
-    }).insertAfter( btn );
+  var loaderContainer = $('<span/>', {
+    'class': 'loader-image-container'
+  }).insertAfter(btn);
 
-    var loader = jQuery( '<img/>', {
-        src: '/wp-admin/images/loading.gif',
-        'class': 'loader-image'
-    }).appendTo( loaderContainer );
+  var loader = $('<img/>', {
+    src: '/wp-admin/images/loading.gif',
+    'class': 'loader-image'
+  }).appendTo(loaderContainer);
 
-	var data = {
-		'action': 'test_dhl_connection'
-	};
+  var data = {
+    'action': 'test_dhl_connection'
+  };
 
-	console.log(data);
-	console.log(dhl_test_con_obj);
+  // We can also pass the url value separately from ajaxurl for front end AJAX implementations
+  $.post(dhl_test_con_obj.ajax_url, data, function(response) {
+    btn.attr('disabled', false);
+    btn.text(response.button_txt);
+    loaderContainer.remove();
 
-	// We can also pass the url value separately from ajaxurl for front end AJAX implementations
-	jQuery.post(dhl_test_con_obj.ajax_url, data, function(response) {
-		btn.attr("disabled", false);
-		btn.text( response.button_txt );
-		loaderContainer.remove();
+    var success = response.connection_success;
+    var test_connection_class = 'dhl_connection_' + (success ? 'succeeded' : 'error');
+    var test_connection_text = success ? response.connection_success : response.connection_error;
 
-		if ( response.connection_success ) {
-			var test_connection_class = 'dhl_connection_succeeded';
-			var test_connection_text = response.connection_success;
-		} else {
-			var test_connection_class = 'dhl_connection_error';
-			var test_connection_text = response.connection_error;
-		}
-		// alert(test_connection_text);
-		loaderContainer = jQuery( '<span/>', {
-	        'class': test_connection_class
-	    }).insertAfter( btn );
+    loaderContainer = $('<span/>', {
+      'class': test_connection_class
+    }).insertAfter(btn);
 
-		loaderContainer.append( test_connection_text );
-	    // jQuery( ).appendTo( loaderContainer );
-		// location.reload();
-	});
+    loaderContainer.append(test_connection_text);
+  });
 }
