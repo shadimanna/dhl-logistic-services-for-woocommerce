87d68fdb00277b534e06d96c790d96f5c33b1691
Shadi Manna <manna.shadi@gmail.com>
2020-03-30 11:34:19 +0200

    eCS Country Setup

--- pr-dhl-woocommerce/includes/pr-dhl-api/class-pr-dhl-api-factory.php
+++ pr-dhl-woocommerce/includes/pr-dhl-api/class-pr-dhl-api-factory.php
@@ -37,24 +37,26 @@ class PR_DHL_API_Factory {
 				case 'PR':
 				case 'UM':
 				case 'VI':
-				case 'CL':
 				case 'CA':
+//                    $dhl_obj = new PR_DHL_API_eCS_US( $country_code );
+//                    break;
+                    throw new Exception( __('The DHL plugin is not supported in your store\'s "Base Location"', 'pr-shipping-dhl') );
 				case 'SG':
 				case 'HK':
 				case 'TH':
-				case 'JP':
+//				case 'JP':
 				case 'CN':
 				case 'MY':
 				case 'VN':
 				case 'AU':
 				case 'IL':
-				case 'NZ':
-				case 'TW':
-				case 'KR':
-				case 'PH':
+//				case 'NZ':
+//				case 'TW':
+//				case 'KR':
+//				case 'PH':
 				case 'IN':
 					//$dhl_obj = new PR_DHL_API_Ecomm( $country_code);
-					$dhl_obj = new PR_DHL_API_eCS_Asia( $country_code);
+					$dhl_obj = new PR_DHL_API_eCS_Asia( $country_code );
 					break;
 				case 'DE':
 					$dhl_obj = new PR_DHL_API_Paket( $country_code );
