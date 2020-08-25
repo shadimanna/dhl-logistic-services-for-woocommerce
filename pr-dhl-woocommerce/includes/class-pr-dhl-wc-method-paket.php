653f9755832df24481fb49a3c399a57fc66679dc
Luthfi Bintoro <iyut85@yahoo.com>
2020-08-25 22:18:14 +0700

    change label format name

--- pr-dhl-woocommerce/includes/class-pr-dhl-wc-method-paket.php
+++ pr-dhl-woocommerce/includes/class-pr-dhl-wc-method-paket.php
@@ -297,11 +297,11 @@ class PR_DHL_WC_Method_Paket extends WC_Shipping_Method {
 				'desc_tip'          => true,
 				'options'           => array( 
 					'A4' => 'A4', 
-					'910-300-700' => '910-300-700', 
-					'910-300-700-oZ' => '910-300-700-oZ', 
-					'910-300-600' => '910-300-600', 
-					'910-300-610' => '910-300-610', 
-					'910-300-710' => '910-300-710' 
+					'910-300-700' => 'Laser printer 105 x 205 mm', 
+					'910-300-700-oZ' => 'Laser printer 105 x 205 mm (no info)', 
+					'910-300-600' => 'Laser printer 105 x 208 mm', 
+					'910-300-610' => 'Thermo printer 103 x 199 mm', 
+					'910-300-710' => 'Thermo printer 103 x 202 mm' 
 				),
 				'class'				=> 'wc-enhanced-select'
 			),
