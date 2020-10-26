b0b9172962bfe12d2adfd6c2d33f6315e6c0d969
Luthfi Bintoro <iyut85@yahoo.com>
2020-10-26 22:15:26 +0700

    add hs code and country manufacture in quick edit

--- pr-dhl-woocommerce/includes/abstract-pr-dhl-wc-product.php
+++ pr-dhl-woocommerce/includes/abstract-pr-dhl-wc-product.php
@@ -25,6 +25,26 @@ abstract class PR_DHL_WC_Product {
 		add_action( 'woocommerce_process_product_meta', array( $this, 'save_additional_product_shipping_options' ) );
 		add_action( 'woocommerce_product_bulk_edit_end', array( $this, 'product_shipping_bulk_edit_input' ) );
 		add_action( 'woocommerce_product_bulk_edit_save', array( $this, 'save_product_shipping_bulk_edit' ) );
+		add_action( 'woocommerce_product_quick_edit_end', array( $this, 'product_shipping_bulk_edit_input' ) );
+		add_action( 'woocommerce_product_quick_edit_save', array( $this, 'save_product_shipping_bulk_edit' ) );
+		add_action( 'manage_product_posts_custom_column', array( $this, 'product_shipping_hidden_input_value'), 20, 2 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'product_enqueue_scripts' ), 20, 1 );
+	}
+
+	public function product_enqueue_scripts( $hook ){
+		
+		$screen = get_current_screen();
+
+		if( 'edit-product' == $screen->id ){
+
+			wp_enqueue_script(
+				'wc-shipment-dhl-product-list-js',
+				PR_DHL_PLUGIN_DIR_URL . '/assets/js/pr-dhl-product-list.js',
+				array( 'jquery' ),
+				PR_DHL_VERSION
+			);
+
+		}
 	}
 
 	/**
@@ -74,7 +94,7 @@ abstract class PR_DHL_WC_Product {
 		$countries = WC()->countries->get_countries();
 	    $countries = array_merge( array('0' => __( '- No change -', 'pr-shipping-dhl' )  ), $countries );
 		?>
-		<div class="inline-edit-group">
+		<div class="inline-edit-group dhl_manufacture_country_inline">
 			<label class="alignleft">
 				<span class="title"><?php _e('Country of Manufacture (DHL)', 'pr-shipping-dhl'); ?></span>
 				<span class="input-text-wrap">
@@ -87,7 +107,7 @@ abstract class PR_DHL_WC_Product {
 			</label>
 		</div>
 
-		<div class="inline-edit-group">
+		<div class="inline-edit-group dhl_hs_code_inline">
 			<label class="alignleft">
 				<span class="title"><?php _e('Harmonized Tariff Schedule (DHL)', 'pr-shipping-dhl'); ?></span>
 				<span class="input-text-wrap">
@@ -124,6 +144,26 @@ abstract class PR_DHL_WC_Product {
 
 		if ( isset( $_REQUEST['change_dhl_manufacture_country'] ) && '0' != $_REQUEST['change_dhl_manufacture_country'] ) {
 			update_post_meta( $post_id, '_dhl_manufacture_country', wc_clean( $_REQUEST['change_dhl_manufacture_country'] ) );
+		}
+	}
+
+	public function product_shipping_hidden_input_value( $column, $post_id ){
+		switch ( $column ) {
+			case 'name' :
+		
+				?>
+				<div class="hidden dhl_hs_code_inline" id="dhl_hs_code_inline_<?php echo $post_id; ?>">
+					<div id="dhl_hs_code"><?php echo get_post_meta($post_id,'_dhl_hs_code',true); ?></div>
+				</div>
+				<div class="hidden dhl_manufacture_country_inline" id="dhl_manufacture_country_inline_<?php echo $post_id; ?>">
+					<div id="dhl_manufacture_country"><?php echo get_post_meta($post_id,'_dhl_manufacture_country',true); ?></div>
+				</div>
+				<?php
+		
+				break;
+		
+			default :
+				break;
 		}
 	}
 
