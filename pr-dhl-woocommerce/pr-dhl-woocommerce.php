4a179296da3a04095e50784c8d744a08fda9bfd4
Shadi Manna <manna.shadi@gmail.com>
2021-07-19 14:07:05 +0200

    WC tested

--- pr-dhl-woocommerce/pr-dhl-woocommerce.php
+++ pr-dhl-woocommerce/pr-dhl-woocommerce.php
@@ -9,7 +9,7 @@
  * Domain Path: /lang
  * Version: 2.5.10
  * WC requires at least: 3.0
- * WC tested up to: 4.5
+ * WC tested up to: 5.1
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
