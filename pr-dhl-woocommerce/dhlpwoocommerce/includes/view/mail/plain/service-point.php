091a502ab18089692add96f5aa24acd7cf9e53a4
Shin Ho <ShinCode@users.noreply.github.com>
2022-03-03 18:08:13 +0000

    Release 2.8.6 (DHL Parcel)

--- pr-dhl-woocommerce/dhlpwoocommerce/includes/view/mail/plain/service-point.php
+++ pr-dhl-woocommerce/dhlpwoocommerce/includes/view/mail/plain/service-point.php
@@ -3,11 +3,12 @@
 /**
  * @var string                        $label
  * @var DHLPWC_Model_API_Data_Address $address
+ * @var string                        $name
  */
 
 if (!defined('ABSPATH')) { exit; }
 
-echo $label . ':' . $name . "\n";
-echo $address->street . "\n";
-echo $address->postal_code . "\n";
-echo $address->country_code . "\n\n";
+echo esc_html($label) . ':' . esc_html($name) . "\n";
+echo esc_html($address->street) . "\n";
+echo esc_html($address->postal_code) . "\n";
+echo esc_html($address->country_code) . "\n\n";
