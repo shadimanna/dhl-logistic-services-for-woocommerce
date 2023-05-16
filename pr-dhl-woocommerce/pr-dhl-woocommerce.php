8c97fc8d9aeae5b8e8b8b174f473f27067ae548c
Abdalsalaam Halawa <abdalsalaam@outlook.com>
2023-05-16 13:37:07 +0300

    version 3.4.1

--- pr-dhl-woocommerce/pr-dhl-woocommerce.php
+++ pr-dhl-woocommerce/pr-dhl-woocommerce.php
@@ -7,7 +7,7 @@
  * Author URI: http://dhl.com/
  * Text Domain: dhl-for-woocommerce
  * Domain Path: /lang
- * Version: 3.4.0
+ * Version: 3.4.1
  * Tested up to: 6.2
  * WC requires at least: 3.0
  * WC tested up to: 7.7
@@ -38,7 +38,7 @@ if ( ! class_exists( 'PR_DHL_WC' ) ) :
 
 class PR_DHL_WC {
 
-	private $version = "3.4.0";
+	private $version = "3.4.1";
 
 	/**
 	 * Instance to call certain functions globally within the plugin
