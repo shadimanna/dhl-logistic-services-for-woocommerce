0a9a312148fa1caed006e96f9eda1cac0a3dedb9
Abdalsalaam Halawa <abdalsalaam@outlook.com>
2025-01-11 17:04:27 +0200

    return empty array instead of string

--- pr-dhl-woocommerce/includes/abstract-pr-dhl-wc-order.php
+++ pr-dhl-woocommerce/includes/abstract-pr-dhl-wc-order.php
@@ -563,7 +563,7 @@ if ( ! class_exists( 'PR_DHL_WC_Order' ) ) :
 			$order = wc_get_order( $order_id );
 
 			if ( ! is_a( $order, 'WC_Order' ) ) {
-				return '';
+				return array();
 			}
 
 			return $order->get_meta( '_pr_shipment_dhl_label_tracking' );
