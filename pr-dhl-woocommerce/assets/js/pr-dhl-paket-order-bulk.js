2d6facee67a62c82efe0076c4c82c1bd3c942ec3
Abdalsalaam Halawa <abdalsalaamnafez@gmail.com>
2025-01-26 03:31:31 +0200

    fix Pickup request HPOS enabled

--- pr-dhl-woocommerce/assets/js/pr-dhl-paket-order-bulk.js
+++ pr-dhl-woocommerce/assets/js/pr-dhl-paket-order-bulk.js
@@ -1,14 +1,12 @@
 ( function( $ ) {
+	var posts_filter = jQuery( '#posts-filter, #wc-orders-filter' );
 
 	var wc_dhl_paket_order_bulk = {
 		// init Class
 		init: function() {
-			$( '#posts-filter' )
-				.on( 'change', '#bulk-action-selector-top', this.toggle_paket_pickup_modal );
-			$( '#posts-filter' )
-				.on( 'change', '#bulk-action-selector-bottom', this.toggle_paket_pickup_modal );
-			$( '#posts-filter' )
-				.on( 'click', '.button.action', this.disable_submit_button );
+			posts_filter.on( 'change', '#bulk-action-selector-top', this.toggle_paket_pickup_modal );
+			posts_filter.on( 'change', '#bulk-action-selector-bottom', this.toggle_paket_pickup_modal );
+			posts_filter.on( 'click', '.button.action', this.disable_submit_button );
 
 			$( '#dhl-paket-action-request-pickup' )
 				.on( 'change', '[name=pr_dhl_request_pickup_modal]', this.show_hide_request_pickup_date_asap );
@@ -25,7 +23,7 @@
 
 			if ( 'pr_dhl_create_labels' === bulkdropdown.val() || 'pr_dhl_delete_labels' === bulkdropdown.val() ) {
 				jQuery( this ).prop( 'disabled', true );
-				jQuery( '#posts-filter' ).submit();
+				posts_filter.submit();
 			}
 		},
 
@@ -34,7 +32,6 @@
 
 			var value 		= jQuery( this ).val();
 			var title 		= jQuery(':selected', this ).text();
-			var post_form 	= jQuery( this ).parents('#posts-filter');
 
 			if( 'pr_dhl_request_pickup' == value ){
 
@@ -70,16 +67,12 @@
 
 			var pickup_type 		= elemModal.find( 'input[name=pr_dhl_request_pickup_modal]:checked' ).val();
 			var pickup_date 		= elemModal.find( 'input[name=pr_dhl_request_pickup_date_modal]' ).val();
-			//var transportation_type = elemModal.find( 'select[name=pr_dhl_request_pickup_transportation_type] :selected' ).val();
 
-			jQuery('#posts-filter [name=dhlpickup]').val(pickup_type);
-			jQuery('#posts-filter [name=dhlpickup_d]').val(pickup_date);
-			//jQuery('#posts-filter [name=dhlpickup_t]').val(transportation_type);
-
-			//console.log('type: '+ pickup_type + ' | date: ' + pickup_date+ ' | transportation_type: ' + transportation_type);
+			jQuery('#posts-filter [name=dhlpickup], #wc-orders-filter [name=dhlpickup]').val(pickup_type);
+			jQuery('#posts-filter [name=dhlpickup_d],#wc-orders-filter [name=dhlpickup_d]').val(pickup_date);
 
 			//Submit bulk action
-			jQuery('#posts-filter #doaction').first().click();
+			jQuery('#posts-filter #doaction, #wc-orders-filter #doaction').first().click();
 
 		}
 	};
