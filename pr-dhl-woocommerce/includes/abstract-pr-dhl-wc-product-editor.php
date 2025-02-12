0ca7278c2de1c5ce04ea5ad916a978d02f6180ca
Abdalsalaam Halawa <abdalsalaamnafez@gmail.com>
2025-02-12 12:32:47 +0400

    format fix

--- pr-dhl-woocommerce/includes/abstract-pr-dhl-wc-product-editor.php
+++ pr-dhl-woocommerce/includes/abstract-pr-dhl-wc-product-editor.php
@@ -46,14 +46,8 @@ if ( ! class_exists( 'PR_DHL_WC_Product_Editor' ) ) :
 		 * Class constructor.
 		 */
 		public function __construct() {
-			add_action( 'woocommerce_block_template_area_product-form_after_add_block_product-shipping-dimensions', array(
-				$this,
-				'add_product_shipping_fields'
-			) );
-			add_action( 'woocommerce_block_template_area_product-form_after_add_block_product-variation-shipping-dimensions', array(
-				$this,
-				'add_product_shipping_fields'
-			) );
+			add_action( 'woocommerce_block_template_area_product-form_after_add_block_product-shipping-dimensions', array( $this, 'add_product_shipping_fields' ) );
+			add_action( 'woocommerce_block_template_area_product-form_after_add_block_product-variation-shipping-dimensions', array( $this, 'add_product_shipping_fields' ) );
 		}
 
 		/**
