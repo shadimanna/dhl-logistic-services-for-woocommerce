0aafe66954ea9abbcb14fbbc120ddb970265daf1
sal4sup <sal4sup@gmail.com>
2025-06-30 22:23:21 +0300

    Fix dhlRetoure and go green

--- pr-dhl-woocommerce/includes/REST_API/Parcel_DE/Item_Info.php
+++ pr-dhl-woocommerce/includes/REST_API/Parcel_DE/Item_Info.php
@@ -824,10 +824,13 @@ class Item_Info {
 			'return_address_enabled' => array(
 				'default' => '',
 				'rename'  => 'dhlRetoure',
-			),
-			'return_go_green_plus'   => array(
-				'default'  => '',
-				'rename'   => 'goGreenPlus',
+				'sanitize' => function ( $args ) use ( $self ) {
+
+					if ( ! empty( $args['go_green_plus'] ) && 'yes' === $args['go_green_plus'] ) {
+						$args['goGreenPlus'] = true;
+					}
+					return $args;
+				},
 			),
 			'signature_service'      => array(
 				'default' => '',
