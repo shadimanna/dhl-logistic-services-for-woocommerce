8689751517da3c74bef58fab195ed16fccf493a1
sal4sup <sal4sup@gmail.com>
2025-07-05 00:46:56 +0300

    fix gmaps api key description placeholders in settings field

--- pr-dhl-woocommerce/includes/class-pr-dhl-wc-method-paket.php
+++ pr-dhl-woocommerce/includes/class-pr-dhl-wc-method-paket.php
@@ -748,12 +748,19 @@ if ( ! class_exists( 'PR_DHL_WC_Method_Paket' ) ) :
 						'title'       => esc_html__( 'API Key', 'dhl-for-woocommerce' ),
 						'type'        => 'text',
 						/* Translators: %s represents a link to obtain a Google Maps API key. */
-						'description' => sprintf(
-							wp_kses(
-								__( 'The Google Maps API Key is necessary to display the DHL Locations on a Google map.<br/>Get a free Google Maps API key %1$s.', 'dhl-for-woocommerce' ),
-								array( 'br' => array() ) // Allow <br> tags
+						'description' => wp_kses(
+							sprintf(
+								__( 'The Google Maps API Key is necessary to display the DHL Locations on a Google map<br/>Get a free Google Maps API key %1$s%2$s', 'dhl-for-woocommerce' ),
+								'<a href="' . esc_url( 'https://developers.google.com/maps/documentation/javascript/get-api-key' ) . '" target="_blank">',
+								esc_html__( 'here', 'dhl-for-woocommerce' ) . '</a>'
 							),
-							'<a href="https://developers.google.com/maps/documentation/javascript/get-api-key" target="_blank">' . esc_html__( 'here', 'dhl-for-woocommerce' ) . '</a>'
+							array(
+								'br' => array(),
+								'a'  => array(
+									'href'   => array(),
+									'target' => array(),
+								),
+							)
 						),
 						'desc_tip'    => false,
 						'class'       => '',
